2d4dc61e2cd4221eb4af50ba373c63e5d72cbf01
bill117752 <98982027+bill117752@users.noreply.github.com>
2022-02-03 13:51:35 -0600

    Add files via upload

---
+++ Free_HTML5_Template_Real_Estate_Website/site/js/script.js
@@ -0,0 +1,31 @@
+// Smooth scroll blocking
+document.addEventListener( 'DOMContentLoaded', function() {
+	if ( 'onwheel' in document ) {
+		window.onwheel = function( event ) {
+			if( typeof( this.RDSmoothScroll ) !== undefined ) {
+				try { window.removeEventListener( 'DOMMouseScroll', this.RDSmoothScroll.prototype.onWheel ); } catch( error ) {}
+				event.stopPropagation();
+			}
+		};
+	} else if ( 'onmousewheel' in document ) {
+		window.onmousewheel= function( event ) {
+			if( typeof( this.RDSmoothScroll ) !== undefined ) {
+				try { window.removeEventListener( 'onmousewheel', this.RDSmoothScroll.prototype.onWheel ); } catch( error ) {}
+				event.stopPropagation();
+			}
+		};
+	}
+
+	try { $('body').unmousewheel(); } catch( error ) {}
+});
+
+$(function(){
+	// faded slider
+	$("#faded").faded({
+		speed: 500,
+		crossfade: true,
+		autoplay: 5000,
+		autorestart: 3000,
+		autopagination:false
+	});
+})
